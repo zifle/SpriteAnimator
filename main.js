05bf1971a30d9ccf5983abc02039578e3a62cb00
Niels Pedersen <nip@emgroup.dk>
2014-11-28 15:35:02 +0100

    ..

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by niels on 11/28/14.
+ */
+
+var sprite = new SpriteAnimator({
+    input: document.querySelector('img.sprite'),
+    frame: {
+        width: 124,
+        height: 93
+    }
+});
--- sprite-animator.js
+++ sprite-animator.js
@@ -5,11 +5,12 @@
 function SpriteAnimator(options) {
     if (typeof options == 'string') {
         options = {
-            'spriteUrl': options
+            'input': options
         };
     } else if (typeof options == 'undefined') options = {};
 
     var defaultOptions = {
+        input: null,
         frame: {
             width: 0,
             height: 0
@@ -29,4 +30,51 @@ function SpriteAnimator(options) {
         if (defaultOptions.hasOwnProperty(i) && options[i] === undefined)
             options[i] = defaultOptions[i];
 
+    this.options = options;
+
+    this.spriteContainer = document.createElement('div');
+
+    if (this.init()) {
+        console.log('Successfully initiated SpriteAnimator');
+    }
 }
+
+SpriteAnimator.prototype.init = function() {
+    if (this.options.input === null) {
+        console.warn('Cannot init SpriteAnimator without input');
+        return false;
+    }
+    var img, self=this;
+    if (typeof this.options.input == 'string') {
+        img = document.createElement('img');
+        img.onload = function() {
+            self.init(); // Run init again, now with the proper element
+        };
+        img.src = this.options.input;
+        document.body.appendChild(img);
+        this.options.input = img;
+        return false;
+    } else if (this.options.input instanceof HTMLImageElement) {
+        img = this.options.input;
+        if (!img.complete) {
+            img.onload = function() {
+                self.init();
+            };
+            return false;
+        }
+    }
+    // Check parameters
+    if (this.options.frame.width == 0 || this.options.frame.height == 0) {
+        console.warn('Cannot run SpriteAnimator without setting frame width/height');
+        return false;
+    }
+
+    this.options.sprite.width = img.width;
+    this.options.sprite.height = img.height;
+
+    this.setSpriteStyling();
+};
+
+SpriteAnimator.prototype.setSpriteStyling = function() {
+
+};
